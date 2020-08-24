bdec15d38de1d4bf6c35f42497481b555c4b03eb
Michael Brink <michael.brink@hotmail.com>
2020-08-24 15:45:12 +1000

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState}   from 'react';
 
-function App() {
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
+
+export default function  App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+     
+    </>
+
   );
 }
 
-export default App;
+
