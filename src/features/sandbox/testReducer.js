9b427ffec284c9b1cafe58bbcb7719595d0ac9ea
Michael Brink <michael.brink@hotmail.com>
2020-09-01 14:38:19 +1000

    End of section 9

--- src/features/sandbox/testReducer.js
+++ src/features/sandbox/testReducer.js
@@ -1,35 +1,53 @@
-const INCREMENT_COUNTER ='INCREMENT_COUNTER';
-const DECREMENT_COUNTER ='DECREMENT_COUNTER';
+import {asyncActionStart, asyncActionFinish, asyncActionError} from '../../app/async/asyncReducer';
+import { delay } from '../../app/common/util/util';
+import {toast} from 'react-toastify';
+
+
+export const INCREMENT_COUNTER ='INCREMENT_COUNTER';
+export const DECREMENT_COUNTER ='DECREMENT_COUNTER';
 
 export function increment(amount) {
-    return {
-    type: INCREMENT_COUNTER,
-    payload: amount
+    return async function(dispatch) {
+        dispatch(asyncActionStart());
+        try {
+            await delay(1000);
+            dispatch({type: INCREMENT_COUNTER, payload: amount});
+            dispatch(asyncActionFinish())
+        } catch (error) {
+            dispatch(asyncActionError(error))
+        }       
     }
 }
 
 export function decrement(amount) {
-    return {
-    type: DECREMENT_COUNTER,
-    payload: amount
+    return async function(dispatch) {
+        dispatch(asyncActionStart());
+        try {
+            await delay(1000);
+            
+            dispatch({type: DECREMENT_COUNTER, payload: amount});
+            dispatch(asyncActionFinish())
+        } catch (error) {
+            dispatch(asyncActionError(error))
+            toast.error(error);
     }
-}
+}}
 
 const initialState = {
-    data: 42
-}
+    data: 42,
+};
 
-export default function testReducer(state = initialState, action) {
-    switch (action.type) {
+export default function testReducer(state = initialState, {type, payload}) {
+    switch (type) {
         case INCREMENT_COUNTER:
             return {
                 ...state,
-                data: state.data + action.payload
+                data: state.data + payload
             };
         case DECREMENT_COUNTER:
             return {
                 ...state,
-                data: state.data - action.payload
+                data: state.data - payload
             };
             default:
                 return state
